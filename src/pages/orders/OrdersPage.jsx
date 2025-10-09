5abdfb9e2b82cfad0a0e594d554e88d2ff74afb4
Timur Khvat <timurkhvat@gmail.com>
2025-10-09 12:28:45 +0200

    Generate the HTML for the orders page

--- src/pages/orders/OrdersPage.jsx
+++ src/pages/orders/OrdersPage.jsx
@@ -1,20 +1,95 @@
+import axios from "axios";
+import dayjs from "dayjs";
+import { useState, useEffect, Fragment } from "react";
 import { Link } from "react-router";
 import { Header } from "../../components/Header";
 import "./OrdersPage.css";
-import buyAgainImg from '../../assets/images/icons/buy-again.png'
+import buyAgainImg from "../../assets/images/icons/buy-again.png";
+import { formatMoney } from "../../utils/money";
 
-export function OrdersPage() {
+export function OrdersPage({ cart }) {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/orders?expand=products").then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
   return (
     <>
       <title>Orders</title>
       <link rel="icon" type="image/svg+xml" href="/orders-favicon.png" />
 
-      <Header />
+      <Header cart={cart} />
 
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
 
         <div className="orders-grid">
+          {orders.map((order) => {
+            return (
+              <div key={order.id} className="order-container">
+                <div className="order-header">
+                  <div className="order-header-left-section">
+                    <div className="order-date">
+                      <div className="order-header-label">Order Placed:</div>
+                      <div>{dayjs(order.orderTimeMS).format("MMMM D")}</div>
+                    </div>
+                    <div className="order-total">
+                      <div className="order-header-label">Total:</div>
+                      <div>{formatMoney(order.totalCostCents)}</div>
+                    </div>
+                  </div>
+
+                  <div className="order-header-right-section">
+                    <div className="order-header-label">Order ID:</div>
+                    <div>{order.id}</div>
+                  </div>
+                </div>
+
+                <div className="order-details-grid">
+                  {order.products.map((orderProduct) => {
+                    return (
+                      <Fragment key={orderProduct.product.id}>
+                        <div className="product-image-container">
+                          <img src={orderProduct.product.image} />
+                        </div>
+
+                        <div className="product-details">
+                          <div className="product-name">
+                            {orderProduct.product.name}
+                          </div>
+                          <div className="product-delivery-date">
+                            Arriving on:{" "}
+                            {dayjs(orderProduct.estimatedDeliveryTimeMs).format(
+                              "MMMM D"
+                            )}
+                          </div>
+                          <div className="product-quantity">
+                            Quantity: {orderProduct.quantity}
+                          </div>
+                          <button className="buy-again-button button-primary">
+                            <img className="buy-again-icon" src={buyAgainImg} />
+                            <span className="buy-again-message">
+                              Add to Cart
+                            </span>
+                          </button>
+                        </div>
+
+                        <div className="product-actions">
+                          <a href="/tracking">
+                            <button className="track-package-button button-secondary">
+                              Track package
+                            </button>
+                          </a>
+                        </div>
+                      </Fragment>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
           <div className="order-container">
             <div className="order-header">
               <div className="order-header-left-section">
@@ -48,10 +123,7 @@ export function OrdersPage() {
                 </div>
                 <div className="product-quantity">Quantity: 1</div>
                 <button className="buy-again-button button-primary">
-                  <img
-                    className="buy-again-icon"
-                    src={buyAgainImg}
-                  />
+                  <img className="buy-again-icon" src={buyAgainImg} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
@@ -77,10 +149,7 @@ export function OrdersPage() {
                 </div>
                 <div className="product-quantity">Quantity: 2</div>
                 <button className="buy-again-button button-primary">
-                  <img
-                    className="buy-again-icon"
-                    src={buyAgainImg}
-                  />
+                  <img className="buy-again-icon" src={buyAgainImg} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
@@ -126,10 +195,7 @@ export function OrdersPage() {
                 </div>
                 <div className="product-quantity">Quantity: 2</div>
                 <button className="buy-again-button button-primary">
-                  <img
-                    className="buy-again-icon"
-                    src={buyAgainImg}
-                  />
+                  <img className="buy-again-icon" src={buyAgainImg} />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
               </div>
