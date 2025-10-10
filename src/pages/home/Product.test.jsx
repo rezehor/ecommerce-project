23fdda72bb956cdaba0a01c769136430a15593f2
Timur Khvat <timurkhvat@gmail.com>
2025-10-10 10:45:29 +0200

    Use beforeEach to remove duplication

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { Product } from "./Product";
 import { render, screen } from "@testing-library/react";
 import axios from "axios";
@@ -7,8 +7,12 @@ import userEvent from "@testing-library/user-event";
 vi.mock("axios");
 
 describe("Product", () => {
-  it("displays the product details correctly", () => {
-    const product = {
+  let product;
+
+  let loadCart;
+
+  beforeEach(() => {
+    product = {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -20,9 +24,11 @@ describe("Product", () => {
       keywords: ["socks", "sports", "apparel"],
     };
 
-    const loadCart = vi.fn();
-    render(<Product product={product} loadCart={loadCart} />);
+    loadCart = vi.fn();
+  });
 
+  it("displays the product details correctly", () => {
+    render(<Product product={product} loadCart={loadCart} />);
     expect(
       screen.getByText("Black and Gray Athletic Cotton Socks - 6 Pairs")
     ).toBeInTheDocument();
@@ -41,22 +47,7 @@ describe("Product", () => {
   });
 
   it("adds a product to the cart", async () => {
-    const product = {
-      id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-      name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      rating: {
-        stars: 4.5,
-        count: 87,
-      },
-      priceCents: 1090,
-      keywords: ["socks", "sports", "apparel"],
-    };
-
-    const loadCart = vi.fn();
-
     render(<Product product={product} loadCart={loadCart} />);
-
     const user = userEvent.setup();
 
     const addToCartButton = screen.getByTestId("add-to-cart-button");
